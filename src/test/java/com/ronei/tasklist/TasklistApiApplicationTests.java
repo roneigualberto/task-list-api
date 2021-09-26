e6dec3fd7834977c9433654e1f5cbe8dc1cc7300
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2021-09-26 14:59:07 -0400

    Project Setup

---
+++ src/test/java/com/ronei/tasklist/TasklistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronei.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
